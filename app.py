4f7dd2b7a558294170a2f3b4ff20cbc3b115af1b
Geoff Matis <gmatis@gmail.com>
2023-09-06 12:36:28 -0400

    more

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-dir_name = "images/path"
+dir_name = "/Users/geoffreymatis/personal_github_page/geofftm.github.io/js/images"
 images_path = os.listdir(dir_name)
 
 image_list = []
@@ -10,8 +10,13 @@ for index, image in enumerate(images_path):
         image_list.append(f"'js/images/{image}'")
         # image_list.append(os.path.join(dir_name, image)) 
 
-with open("js/loadImages.js", "w") as f:
-    f.write("var images = [")
+
+with open("/Users/geoffreymatis/personal_github_page/geofftm.github.io/js/loadImages.js", "w") as f:
+    f.write("randomImage()\n")
+    f.write("\n")
+    f.write("function randomImage() {")
+    f.write("\n")
+    f.write("   var images = [")
     for index, i in enumerate(image_list):
         if index != (len(image_list) - 1):
             f.write(f"{i}, ")
@@ -19,11 +24,15 @@ with open("js/loadImages.js", "w") as f:
             f.write(f"{i}")
     f.write("]")
     f.write("\n")
-    f.write("var randomImageSrc = images[Math.floor(Math.random() * images.length)];")
+    f.write("   var randomImageSrc = images[Math.floor(Math.random() * images.length)];")
     f.write("\n")
-    f.write('''var image = "<a href='index.html'><img src='" + randomImageSrc + "'></a>";''')
+    f.write(    '''var image = "<a href='index.html'><img src='" + randomImageSrc + "'></a>";''')
     f.write("\n")
-    f.write("document.getElementById('img-load').innerHTML = image;")
+    f.write("   document.getElementById('img-load').innerHTML = image;")
+    f.write("\n")
+    f.write("   window.setInterval(randomImage, 60000);")
+    f.write("\n")
+    f.write("}")
     f.close()
 
     
